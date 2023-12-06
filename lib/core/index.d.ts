1e8f6a7f421696733d6d5234e1ed21756e269716
lZiMUl <AA2908554069@gmail.com>
2023-12-06 08:41:14 -0800

    New Project

--- lib/core/index.d.ts
+++ lib/core/index.d.ts
@@ -1,17 +1,21 @@
-import { Callback, Config, Event, FilesFormat, FilesInfo, ModFormat, ModInfo } from '../interfaces';
+import {Callback, Config, Event, FilesFormat, FilesInfo, ModFormat, ModInfo} from '../interfaces';
+
 declare class UpdateMods {
     private readonly event;
     private readonly modInfo;
     private readonly mods;
     private readonly instance;
     private filesData;
-    constructor(filePath: string, config: Config);
-    addListener<T>(event: Event, callback: Callback<T>): void;
     private update;
     private downloadFile;
     private createFile;
     private writeManifest;
+
+    constructor(filePath: string, config: Config);
+
+    addListener<T>(event: Event, callback: Callback<T>): void;
 }
+
 export default UpdateMods;
-export type { FilesFormat, ModFormat, ModInfo, FilesInfo, Event, Callback };
+export type {FilesFormat, ModFormat, ModInfo, FilesInfo, Event, Callback};
 //# sourceMappingURL=index.d.ts.map
--- lib/interfaces/index.d.ts
+++ lib/interfaces/index.d.ts
@@ -3,26 +3,31 @@ interface Config {
     forceDownload?: boolean;
     outDir?: string;
 }
+
 interface ModFormat {
     projectID: number;
     fileID: number;
     required: boolean;
 }
+
 interface ModInfo extends ModFormat {
     id: number;
     modId: number;
     fileName: string;
     downloadUrl: string | undefined;
 }
+
 interface FilesFormat {
     files: ModFormat[];
     data: ModInfo[];
 }
+
 interface FilesInfo {
     succeed: ModFormat[];
     fail: ModFormat[];
 }
+
 type Event = 'download' | 'downloaded' | 'skipped' | 'done' | 'error';
 type Callback<T> = (mods: T) => void;
-export type { Config, ModFormat, ModInfo, FilesFormat, FilesInfo, Event, Callback };
+export type {Config, ModFormat, ModInfo, FilesFormat, FilesInfo, Event, Callback};
 //# sourceMappingURL=index.d.ts.map
