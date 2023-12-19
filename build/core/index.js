e8502576c03e29b64a6d2d01ed007da6b74a5d65
lZiMUl <AA2908554069@gmail.com>
2023-12-18 21:05:37 -0800

    My new Project

---
+++ build/core/index.js
@@ -0,0 +1,107 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {value: true});
+const node_path_1 = require("node:path");
+const node_events_1 = require("node:events");
+const node_fs_1 = require("node:fs");
+const axios_1 = require("axios");
+const node_process_1 = require("node:process");
+
+class UpdateMods {
+    event = new node_events_1.EventEmitter();
+    modInfo;
+    mods;
+    instance;
+    filesData = {
+        succeed: [],
+        fail: []
+    };
+
+    constructor(filePath, config) {
+        this.instance = (0, axios_1.create)({
+            baseURL: 'https://api.curseforge.com/v1/mods',
+            method: 'GET',
+            params: {
+                gameVersion: '1.20.1',
+                modLoaderType: 1
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'x-api-key': config.api
+            }
+        });
+        this.modInfo = JSON.parse((0, node_fs_1.readFileSync)(filePath, {
+            encoding: 'utf-8'
+        }));
+        this.mods = [...new Set(this.modInfo.files.map(({projectID, fileID, required}) => ({
+            projectID, fileID, required
+        })))];
+        this.update(config);
+    }
+
+    addListener(event, callback) {
+        this.event.addListener(event, callback);
+    }
+
+    update(config, {projectID, fileID, required} = this.mods.shift()) {
+        if (required) {
+            setTimeout(async () => {
+                if (this.mods.length && projectID && fileID) {
+                    const {data} = await this.instance.request({
+                        url: `${projectID}/files`,
+                    });
+                    let mods = Object.assign({fileID}, data.data[0]);
+                    if (mods.id !== fileID || config.forceDownload) {
+                        if (mods.downloadUrl) {
+                            this.event.emit('download', mods);
+                            await this.downloadFile(mods, (0, node_path_1.join)(config.outDir || (0, node_path_1.resolve)('.'), 'MinecraftModsUpdate'), config);
+                        } else {
+                            this.event.emit('error', mods);
+                            this.writeManifest(mods, false);
+                            this.update(config);
+                        }
+                    } else {
+                        this.event.emit('skipped', mods);
+                        this.writeManifest(mods, true);
+                        this.update(config);
+                    }
+                } else {
+                    (0, node_fs_1.writeFileSync)('MinecraftModsUpdate.json', JSON.stringify(this.filesData, null, 2));
+                    this.event.emit('done', this.filesData);
+                    (0, node_process_1.exit)(0);
+                }
+            }, 2000);
+        }
+    }
+
+    async downloadFile(mods, path, config) {
+        const {data} = await (0, axios_1.request)({
+            url: mods.downloadUrl,
+            responseType: 'stream'
+        });
+        data.pipe(this.createFile(mods.fileName, path));
+        this.writeManifest(mods, true);
+        this.event.emit('downloaded', mods);
+        this.update(config);
+    }
+
+    createFile(fileName, path) {
+        if (!(0, node_fs_1.existsSync)(path))
+            (0, node_fs_1.mkdirSync)(path);
+        return (0, node_fs_1.createWriteStream)((0, node_path_1.join)(path, fileName));
+    }
+
+    writeManifest(mods, status) {
+        const modsInfo = {
+            projectID: mods.modId,
+            fileID: mods.id,
+            required: true
+        };
+        if (status)
+            this.filesData.succeed.push(modsInfo);
+        else
+            this.filesData.fail.push(modsInfo);
+    }
+}
+
+exports.default = UpdateMods;
---
+++ build/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const node_process_1 = require("node:process");
+const node_console_1 = require("node:console");
+const chalk_1 = require("chalk");
+const index_1 = tslib_1.__importDefault(require("./core/index"));
+const modUpdate = new index_1.default(node_process_1.argv.slice(2)[0], {
+    api: '$2a$10$wBnbjNo6Ue9pR8hVruq3T.Ig2cm4ECOppRsmYIJh09v8xeEb6gJi2'
+});
+modUpdate.addListener('download', (mods) => (0, node_console_1.info)(`${(0, chalk_1.magentaBright)('Downloading:')} ${(0, chalk_1.blueBright)(mods.fileName)} {(${(0, chalk_1.yellowBright)(mods.modId)}) [${(0, chalk_1.redBright)(mods.fileID)} => ${(0, chalk_1.greenBright)(mods.id)}]} -> ${(0, chalk_1.blueBright)(mods.downloadUrl)}`));
+modUpdate.addListener('downloaded', (mods) => (0, node_console_1.info)(`${(0, chalk_1.greenBright)('The download is complete')}: ${(0, chalk_1.blueBright)(mods.fileName)}\n`));
+modUpdate.addListener('skipped', (mods) => (0, node_console_1.warn)(`${(0, chalk_1.greenBright)('Already the latest version, the update has been skipped')}: ${(0, chalk_1.blueBright)(mods.fileName)} (${(0, chalk_1.yellowBright)(mods.modId)} [${(0, chalk_1.greenBright)(mods.fileID)} == ${(0, chalk_1.greenBright)(mods.id)}]) \n`));
+modUpdate.addListener('done', (mods) => (0, node_console_1.info)(mods, '\n', (0, chalk_1.greenBright)('The update is complete')));
+modUpdate.addListener('error', (mods) => (0, node_console_1.error)(`${(0, chalk_1.redBright)('=====Error: Unable to get file address, please download it manually=====')}\nMod ID: ${(0, chalk_1.yellowBright)(mods.modId)}\nThe name of the mod file: ${(0, chalk_1.magentaBright)(mods.fileName)}\n`));
