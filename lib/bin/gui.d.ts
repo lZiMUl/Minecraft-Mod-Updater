d0a7841a8af5305dcfdbd156c90d19daedb480bf
lZiMUl <AA2908554069@gmail.com>
2024-01-07 14:42:29 -0800

    Fixed Project

--- lib/bin/gui.d.ts
+++ lib/bin/gui.d.ts
@@ -1,2 +1,3 @@
+#!/usr/bin/env electron
 export {};
 //# sourceMappingURL=gui.d.ts.map
--- src/bin/gui.ts
+++ src/bin/gui.ts
@@ -1,1 +1,26 @@
-throw new Error('Not yet developed, stay tuned');
+#!/usr/bin/env electron
+import { app, BrowserWindow, BrowserWindowConstructorOptions } from 'electron';
+
+class GUI extends BrowserWindow {
+    private status: boolean = false;
+    public constructor(config: BrowserWindowConstructorOptions) {
+        super(config);
+        if(this.status) {
+            super.loadURL('https://lzimul.top/archives/b628cc84-7903-4ecd-8205-8626dba18f68');
+        } else {
+            throw new Error('Not yet developed, stay tuned');
+        }
+    }
+}
+
+(async (): Promise<void> => {
+    try {
+        await app.whenReady();
+        new GUI({
+            'width': 800,
+            'height': 600
+        });
+    } catch (err) {
+        throw new Error('Not yet developed, stay tuned');
+    }
+})();
