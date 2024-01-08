e2251ddac166ccc959ca77712fd3230511ca966f
lZiMUl <AA2908554069@gmail.com>
2024-01-08 11:52:14 -0800

    Update Project And Add GUI

--- lib/bin/gui.d.ts
+++ lib/bin/gui.d.ts
@@ -1,3 +1,3 @@
-#!/node_modules/minecraft-mod-updater/node_modules/electron/dist/electron.exe
+#!/node_modules/minecraft-mod-updater/node_modules/electron/dist/electron
 export {};
 //# sourceMappingURL=gui.d.ts.map
